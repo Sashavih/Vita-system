fb9f22d60e4b14ccf8bd1d9e3bccd179806300cb
Sasha <sashavikhorevaa@gmail.com>
2023-08-10 13:37:31 +0300

    feat: add tablet version

---
+++ assets/scripts/buttle-click.js
@@ -0,0 +1,12 @@
+$('body').on('click', '#active-buttle', function(e){
+    document.location.href = 'index.html#active';
+}
+)
+$('body').on('click', '#mint', function(e){
+    document.location.href = 'index.html#detox';
+}
+)
+$('body').on('click', '#immuno-buttle', function(e){
+    document.location.href = 'index.html#immuno';
+}
+)
--- assets/scripts/show-text.js
+++ assets/scripts/show-text.js
@@ -19,3 +19,4 @@
     $('.title-animation').fadeIn(1200);
    
 })
+
